f78325d1fc398227e848ec139ef93d8e8d046056
Joris de Wit <joris.w.dewit@gmail.com>
2015-05-14 12:07:16 -0700

    big refactor
    - use render to update checked state
    - use ng-click to handle toggle
    - minify css
    - remove unecessary styles
    - simply and clean up project structure
    - update readme & demo
    - add some Bootstrap 3 form styling

---
+++ src/ez-checkbox.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('ez.checkbox', [])
+.constant('EzCheckboxConfig', {
+  'iconClass': 'glyphicon glyphicon-ok'
+})
+.directive('ezCheckbox', ['EzCheckboxConfig', function(EzCheckboxConfig) {
+	return {
+		require: 'ngModel',
+		restrict: 'EA',
+		replace: 'true',
+		transclude: true,
+		template: '<a class="ez-checkbox" ng-click="toggle()">' +
+                '<span class="check-box">' +
+                  '<i class="{{options.iconClass}}" ng-show="checked"></i>' +
+                '</span>' +
+                '<span class="check-label" ng-transclude></span>' +
+              '</a>',
+    scope: {},
+		link: function(scope, elem, attrs, modelCtrl) {
+			var trueValue = true;
+			var falseValue = false;
+
+      scope.options = EzCheckboxConfig;
+
+			if (attrs.hasOwnProperty('trueValue')) {
+				trueValue = attrs.trueValue;
+			}
+
+      // invert truthyness if invert is defined
+      if (attrs.hasOwnProperty('invert')) {
+        falseValue = trueValue;
+        trueValue = false;
+      }
+
+      modelCtrl.$render = function() {
+        if (modelCtrl.$modelValue === trueValue) {
+          scope.checked = true;
+        } else {
+          scope.checked = false;
+        }
+      };
+
+      scope.toggle = function() {
+        modelCtrl.$setViewValue(modelCtrl.$modelValue === trueValue ? falseValue : trueValue);
+
+        modelCtrl.$render();
+			};
+		}
+	};
+}]);
